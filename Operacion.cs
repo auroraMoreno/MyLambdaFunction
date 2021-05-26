3206ae774a8d76ef6554e2faf65857f0c59a70bb
Aurora Moreno Lopez <aurora.moreno.lopez@tajamar365.com>
2021-05-26 08:47:17 +0200

    Add project files.

---
+++ Operacion.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyLambdaFunction
+{
+    public class Operacion
+    {
+        public int Numero1 { get; set; }
+        public int Numero2 { get; set; }
+    }
+}
